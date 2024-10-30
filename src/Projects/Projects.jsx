9172778c72aa288927965d91cbf53998167e3ddd
bgar324 <bentgarcia05@gmail.com>
2024-10-30 01:16:13 -0700

    finished project page

--- src/Projects/Projects.jsx
+++ src/Projects/Projects.jsx
@@ -17,36 +17,59 @@ const Projects = () => {
     </div>
     <div className="p-mastercontainer">
       <ProjectsContainer
-        projectName = "adsfs"
-        projectImageSrc = "\static\images\cscclubgameflyerv2.png"
-        projectLink = "youtube.com"
+        projectName="Intro to Arduino"
+        projectLink="https://docs.google.com/presentation/d/1ibq6mKTArqkUY7aMJD_wPrBJIE6gd7xQx00o1-9ZVcc/edit#slide=id.p"
       />
       <ProjectsContainer
-        projectName = "adsfs"
-        projectImageSrc = "\static\images\cscclubgameflyerv2.png"
-        projectLink = "youtube.com"
+        projectName="Intro to Computer Science"
+        projectLink="https://docs.google.com/presentation/d/1qVwx4yZ5tW8dbVJDzh1pRuDjeKLZZMLt0PGaoFUd-n0/edit?usp=sharing"
       />
       <ProjectsContainer
-        projectName = "adsfs"
-        projectImageSrc = "\static\images\cscclubgameflyerv2.png"
-        projectLink = "youtube.com"
-      />            <ProjectsContainer
-      projectName = "adsfs"
-      projectImageSrc = "\static\images\cscclubgameflyerv2.png"
-      projectLink = "youtube.com"
-    />      
-          <ProjectsContainer
-        projectName = "adsfs"
-        projectImageSrc = "\static\images\cscclubgameflyerv2.png"
-        projectLink = "youtube.com"
-      />      
-            <ProjectsContainer
-        projectName = "adsfs"
-        projectImageSrc = "\static\images\cscclubgameflyerv2.png"
-        projectLink = "youtube.com"
-      /> 
-    </div>
+        projectName="Citrus Hack 2019"
+        projectLink="https://docs.google.com/presentation/d/1kvQQ9zwFHZ3iwAoByCs4mibPTC9Upagi8DU0oIBx3RY/edit?usp=sharing"
+      />
+      <ProjectsContainer
+        projectName="Intro to Git and Github"
+        projectLink="https://docs.google.com/presentation/d/1xuTi38wjr2_dkuBIDsY_mwUNmLzFm_gIkvDvZDcVCEQ/edit?usp=sharing"
+      />
+      <ProjectsContainer
+        projectName="Resume Workshop"
+        projectLink="https://docs.google.com/presentation/d/1K-zcJryfaTtuF6QDq6YsgikRWzYZ_2L1UAnH8GG_WBc/edit?usp=sharing"
+      />
+      <ProjectsContainer
+        projectName="Intro to Java"
+        projectLink="https://docs.google.com/presentation/d/1mkV-e8Umj3ohlOdyqsSIXpQAvhg5J3osSbl5Z_efK3c/edit#slide=id.p"
+      />
+      <ProjectsContainer
+        projectName="Mt. Hacks 2019"
+        projectLink="https://docs.google.com/presentation/d/16CZ6Xkcd68mVwhtWgw9XtgK--Ar2U6PZ1aphV6WcviA/edit#slide=id.p"
+      />
+      <ProjectsContainer
+        projectName="App building with Mendix"
+        projectLink="https://docs.google.com/presentation/d/1TDU9_XIL-FdNbVrhpzmsge8rzIhpQ-2KUVmxTZprgBQ/edit?usp=sharing"
+      />
+      <ProjectsContainer
+        projectName="AI Briefing"
+        projectLink="https://docs.google.com/presentation/d/19D-eFgYFcaV79_DuUVP9HVPauB9pX6LQ6jmz-KFvQpE/edit?usp=sharing"
+      />
+      <ProjectsContainer
+        projectName="Web Development"
+        projectLink="https://docs.google.com/presentation/d/1n_b8fKJWwiZZnUXX9GhMSezJvULtRHDlOmDYwAMhOrw/edit?usp=sharing"
+      />
+      <ProjectsContainer
+        projectName="Introduction to Web Dev"
+        projectLink="https://docs.google.com/presentation/d/1JWuLIgnXQyr_Ez8E3F6-4owPj42K8vZ3UtO_nMp2J3A/edit?usp=sharing"
+      />
+      {/* <ProjectsContainer
+        projectName="Mt. Hacks 2019"
+        projectLink="https://docs.google.com/presentation/d/16CZ6Xkcd68mVwhtWgw9XtgK--Ar2U6PZ1aphV6WcviA/edit#slide=id.p"
+      />
+      <ProjectsContainer
+        projectName="Mt. Hacks 2019"
+        projectLink="https://docs.google.com/presentation/d/16CZ6Xkcd68mVwhtWgw9XtgK--Ar2U6PZ1aphV6WcviA/edit#slide=id.p"
+      /> */}
 
+    </div>
     <Footer />
     </>
   )
--- src/Projects/ProjectsContainer.jsx
+++ src/Projects/ProjectsContainer.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import './ProjectsContainer.css';
 
-const ProjectsContainer = ({ projectName, projectImageSrc, projectLink }) => {
+const ProjectsContainer = ({ projectName, projectLink }) => {
   return (
-    <>
     <div className="pc-master">
-      <a href={projectLink} targe="_blank" rel="noopener noreferrer" className="pc-link">
+      <a href={projectLink} target="_blank" rel="noopener noreferrer" className="pc-link">
         <div className="pc-header">
           <h3>{projectName}</h3>
         </div>
-        <div className="pc-image">
-          <img src={projectImageSrc} alt={`${projectName} thumbnail`} />
+        <div className="pc-content">
+          <iframe
+            src={`${projectLink.replace('/edit', '/preview')}`}
+            title={`${projectName} presentation`}
+            className="pc-iframe"
+            allowFullScreen
+          ></iframe>
         </div>
       </a>
     </div>
-    </>
   );
 };
+
 export default ProjectsContainer;
