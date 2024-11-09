e129510232d55267a78c8e98356dc30fa98d456e
bgar324 <bentgarcia05@gmail.com>
2024-11-08 23:12:01 -0800

    hero v1

--- src/HomePage/Home.jsx
+++ src/HomePage/Home.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import Navbar from './Navbar.jsx'
+import React from 'react';
+import Navbar from './Navbar.jsx';
 import Footer from './Footer.jsx';
-import './Home.css'
-import { Helmet } from 'react-helmet'; 
+import './Home.css';
+import { Helmet } from 'react-helmet';
 
 const Home = () => {
   return (
@@ -11,9 +11,13 @@ const Home = () => {
         <title>Home | CS Club at Mt. SAC</title>
       </Helmet>        
       <Navbar />
+      <div className="hero-header">
+        <img src="public/static/images/Mt SAC Buildings_43.jpg" alt="Building 43" className="background-image"/>
+        <img src="public/static/images/MtSAC (1).png" alt="Duck Logo" className="logo-image"/>
+      </div>
       <Footer />
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
