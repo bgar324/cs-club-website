f34a6866f3e8ab75e87a92f83050c9d53eef2dce
bgar324 <bentgarcia05@gmail.com>
2024-11-09 01:59:37 -0800

    did a lot ummm most of the landing page, just have to do text boxes

--- src/HomePage/Home.jsx
+++ src/HomePage/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Navbar from './Navbar.jsx';
 import Footer from './Footer.jsx';
 import './Home.css';
@@ -11,10 +12,33 @@ const Home = () => {
         <title>Home | CS Club at Mt. SAC</title>
       </Helmet>        
       <Navbar />
-      <div className="hero-header">
-        <img src="public/static/images/Mt SAC Buildings_43.jpg" alt="Building 43" className="background-image"/>
-        <img src="public/static/images/MtSAC (1).png" alt="Duck Logo" className="logo-image"/>
+      <div className="home-master">
+        <div className="hero-header">
+          <img src="public/static/images/Mt SAC Buildings_43.jpg" alt="Building 43" className="background-image"/>
+          <img src="public/static/images/MtSAC (1).png" alt="Duck Logo" className="logo-image"/>
+        </div>
+
+        <div className="what-is">
+          <div className="what-is-text">
+           <h2>What is Computer Science at Mt. SAC?</h2>
+           <p>Computer science is the study of computers, computation, information, and automation, encompassing their design, development, and practical applications. It includes a focus on creating and testing software and systems, from websites (like this one!) to complex, automated solutions. Learn more at the <a href="https://www.mtsac.edu/math/" target="_blank">Math and Computer Science department</a> for coursework and career paths.</p>
+           <p>To learn our origins and what we offer, check out the about page. </p>
+           <Link to ="/about" className="hero-button">about</Link>
+           {/* ADD ICONS HYPERLINKS HERE */}
+          </div>
+          <img src = "public\static\images\IMG_4719.png" className= "what-is-image" />
+        </div>
       </div>
+
+      <div className="hero-header-2">
+        <img src ="public/static/images/banner2.png" className ="banner2"/>
+      </div>
+
+      <div className="etc-text">
+        
+      </div>
+
+      
       <Footer />
     </>
   );
